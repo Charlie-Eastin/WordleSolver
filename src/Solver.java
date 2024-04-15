37a74e34a7dbd1fe9dbdc0c22f0d9317d7f3e465
ChrisWhisker <chrisworc@gmail.com>
2024-04-14 22:35:33 -0400

    Add main Solver class

---
+++ src/Solver.java
@@ -0,0 +1,28 @@
+/**
+ * The main Wordle-solver class, containing methods for each algorithm as well as thread management
+ */
+public class Solver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+	// Solve Wordle using CSP / genetic algorithm
+	private static void GeneticAlg() {
+
+	}
+
+	// Solve Wordle using the basic brute force algorithm
+	private static void BruteForceBasic() {
+
+	}
+
+	// Solve Wordle using the smarter force algorithm
+	private static void BruteForceSmart() {
+
+	}
+
+}
