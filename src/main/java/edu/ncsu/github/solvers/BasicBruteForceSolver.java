228014d94cc33cc3616d8d4231b978f114cca0f0
ChrisWhisker <cdworces@ncsu.edu>
2024-04-17 13:43:49 -0400

    Move advanced brute force code to correct class

--- src/main/java/edu/ncsu/github/solvers/BasicBruteForceSolver.java
+++ src/main/java/edu/ncsu/github/solvers/BasicBruteForceSolver.java
@@ -1,107 +1,20 @@
 package edu.ncsu.github.solvers;
 
-import edu.ncsu.github.wordle.Letter;
 import edu.ncsu.github.wordle.Word;
-import edu.ncsu.github.wordle.WordLengthMismatchException;
 
-import java.util.ArrayList;
-import java.util.List;
-
-// Solver implementing the basic brute force algorithm
+// Solver implementing the basic brute force algorithm, described as "Isolated Brute Force" in the project proposal
 public class BasicBruteForceSolver implements Solver {
 
-	// All letters that have not been eliminated as possibly being in the solution
-	private final List<Character> alphabet = new ArrayList<>();
 	// Tracks the current guess along with each letter's status
 	private Word guess;
 
-	// Instance-initialize alphabet
-	{
-		// Populate alphabet with all uppercase letters
-		for (char c : "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()) {
-			alphabet.add(c);
-		}
-	}
-
-	/**
-	 * Solve the Wordle puzzle.
-	 *
-	 * @param solutionLength The length of the solution word.
-	 */
 	@Override
-	public void solve(int solutionLength) throws WordLengthMismatchException {
-		// Initialize the guess word with the given length
+	public void solve(int solutionLength) {
 		guess = new Word(solutionLength);
 
-		// Call the recursive function to generate combinations and check against the solution
-		boolean solutionFound = generateGuesses();
-
-		// If no solution is found, print a message
-		if (!solutionFound) {
-			System.out.println("Solution not found");
-		}
+		System.out.println("Advanced brute force algorithm not yet implemented");
+		// TODO: Implement smarter brute force algorithm to solve Wordle
 	}
 
-	/**
-	 * Generate all combinations of words to solve the puzzle.
-	 *
-	 * @return True if a solution is found, false otherwise.
-	 */
-	private boolean generateGuesses() throws WordLengthMismatchException {
-		// Check if the current guess matches the solution
-		if (guess.compareToSolution()) {
-			System.out.println("Solution found: " + guess);
-			return true;
-		}
-
-		// Iterate through each letter in the guess
-		for (int i = 0; i < guess.getLength(); i++) {
-			Letter guessLetter = guess.getLetterAt(i);
-
-			// Check the status of the current letter
-			switch (guessLetter.getStatus()) {
-				case GREEN_CORRECT:
-					break;
-				case GRAY_NONEXISTENT:
-					// Remove the letter from the alphabet since it's not in the solution
-					alphabet.remove((Character) guessLetter.getCharacter());
-					// Don't break so execution flows into YELLOW case.
-				case YELLOW_MISPLACED:
-					// Replace the letter with the next valid character
-					Letter replacement = new Letter(nextValidChar(guessLetter.getCharacter()));
-					guess.setLetter(i, replacement);
-					break;
-				case WHITE_UNKNOWN:
-					// Throw an exception if the letter has not been evaluated
-					throw new RuntimeException("Letter has not been evaluated.");
-				default:
-					// Throw an exception for unsupported statuses
-					throw new RuntimeException("RED and ORANGE status not yet supported.");
-			}
-		}
-		// Recursive call to generate more guesses
-		return generateGuesses();
-	}
-
-	/**
-	 * Get the next valid character to replace the current one.
-	 *
-	 * @param original The original character.
-	 * @return The next valid character.
-	 */
-	private char nextValidChar(char original) {
-		if (original == 'Z') {
-			// Cannot replace 'Z' with the next letter
-			throw new RuntimeException("Cannot replace 'Z' with the next letter");
-		} else {
-			char nextChar = original;
-			// Increment the character to get the next letter
-			// TODO Prioritize letters we know are in the word (yellow)
-			do {
-				nextChar = (char) (nextChar + 1);
-			} while (!alphabet.contains(nextChar));
-
-			return nextChar;
-		}
-	}
+	// TODO Add private helper methods below
 }
