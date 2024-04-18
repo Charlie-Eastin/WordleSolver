14d779244cb42a435c2774832d1cf6b968cb9200
ChrisWhisker <cdworces@ncsu.edu>
2024-04-18 14:58:11 -0400

    Move Letter handling into abstract brute force class

--- src/main/java/edu/ncsu/github/solvers/AdvBruteForceSolver.java
+++ src/main/java/edu/ncsu/github/solvers/AdvBruteForceSolver.java
@@ -1,6 +1,5 @@
 package edu.ncsu.github.solvers;
 
-import edu.ncsu.github.wordle.Letter;
 import edu.ncsu.github.wordle.Word;
 import edu.ncsu.github.wordle.WordLengthMismatchException;
 
@@ -33,7 +32,7 @@ public class AdvBruteForceSolver extends BruteForceSolver {
 	 * @return True if a solution is found, false otherwise.
 	 */
 	private boolean generateGuesses() throws WordLengthMismatchException {
-		// Check if the current guess matches the solution
+		// Check if the current guess matches the solution and give each Letter a status
 		if (guess.compareToSolution()) {
 			System.out.println("Solution found: " + guess);
 			return true;
@@ -41,28 +40,7 @@ public class AdvBruteForceSolver extends BruteForceSolver {
 
 		// Iterate through each letter in the guess
 		for (int i = 0; i < guess.getLength(); i++) {
-			Letter guessLetter = guess.getLetterAt(i);
-
-			// Check the status of the current letter
-			switch (guessLetter.getStatus()) {
-				case GREEN_CORRECT:
-					break;
-				case GRAY_NONEXISTENT:
-					// Remove the letter from the alphabet since it's not in the solution
-					alphabet.remove((Character) guessLetter.getCharacter());
-					// Don't break so execution flows into YELLOW case.
-				case YELLOW_MISPLACED:
-					// Replace the letter with the next valid character
-					Letter replacement = new Letter(nextValidChar(guessLetter.getCharacter()));
-					guess.setLetter(i, replacement);
-					break;
-				case WHITE_UNKNOWN:
-					// Throw an exception if the letter has not been evaluated
-					throw new RuntimeException("Letter has not been evaluated.");
-				default:
-					// Throw an exception for unsupported statuses
-					throw new RuntimeException("RED and ORANGE status not yet supported.");
-			}
+			handleLetterAt(i);
 		}
 		// Recursive call to generate more guesses
 		return generateGuesses();
--- src/main/java/edu/ncsu/github/solvers/BruteForceSolver.java
+++ src/main/java/edu/ncsu/github/solvers/BruteForceSolver.java
@@ -1,5 +1,6 @@
 package edu.ncsu.github.solvers;
 
+import edu.ncsu.github.wordle.Letter;
 import edu.ncsu.github.wordle.Word;
 import edu.ncsu.github.wordle.WordLengthMismatchException;
 
@@ -26,6 +27,31 @@ public abstract class BruteForceSolver implements Solver {
 	@Override
 	public void solve(int solutionLength) throws WordLengthMismatchException {
 		throw new RuntimeException("This method should never be called. You probably meant to call it on one of this class' children.");
+	}
+
+	protected void handleLetterAt(int letterIndex) {
+		Letter guessLetter = guess.getLetterAt(letterIndex);
+
+		// Check the status of the current letter
+		switch (guessLetter.getStatus()) {
+			case GREEN_CORRECT:
+				break;
+			case GRAY_NONEXISTENT:
+				// Remove the letter from the alphabet since it's not in the solution
+				alphabet.remove((Character) guessLetter.getCharacter());
+				// Don't break so execution flows into YELLOW case.
+			case YELLOW_MISPLACED:
+				// Replace the letter with the next valid character
+				Letter replacement = new Letter(nextValidChar(guessLetter.getCharacter()));
+				guess.setLetter(letterIndex, replacement);
+				break;
+			case WHITE_UNKNOWN:
+				// Throw an exception if the letter has not been evaluated
+				throw new RuntimeException("Letter has not been evaluated.");
+			default:
+				// Throw an exception for unsupported statuses
+				throw new RuntimeException("RED and ORANGE status not yet supported.");
+		}
 	}
 
 	/**
