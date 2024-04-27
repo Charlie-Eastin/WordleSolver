7cabfb4f33b765144776d5ce11a70cfa635cd5a8
Chris Worcester <cdworces@ncsu.edu>
2024-04-27 15:30:16 -0400

    Add window title and comments

--- src/main/java/edu/ncsu/github/WordleSolverGUI.java
+++ src/main/java/edu/ncsu/github/WordleSolverGUI.java
@@ -4,7 +4,11 @@ import javax.swing.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
+/**
+ * WordleSolverGUI class represents the graphical user interface for a Wordle solver application.
+ */
 public class WordleSolverGUI {
+	// GUI components
 	private JButton solveButton;
 	private JCheckBox mutationsCheckBox;
 	private JTextField intervalTextField;
@@ -20,10 +24,24 @@ public class WordleSolverGUI {
 	private JPanel mutationsPanel;
 	private JPanel centerPanel;
 
+	/**
+	 * Constructor for WordleSolverGUI class.
+	 * Sets the look and feel to Windows and adds item listeners to check boxes.
+	 */
 	public WordleSolverGUI() {
+		try {
+			// Set the look and feel to Windows
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+				 UnsupportedLookAndFeelException e) {
+			System.out.println("Error while setting GUI look and feel: " + e);
+		}
+
+		// Add item listener to the generate check box
 		generateCheckBox.addItemListener(new ItemListener() {
 			@Override
 			public void itemStateChanged(ItemEvent e) {
+				// Enable/disable text fields based on check box selection
 				switch (e.getStateChange()) {
 					case ItemEvent.SELECTED:
 						lettersTextField.setEnabled(true);
@@ -36,9 +54,12 @@ public class WordleSolverGUI {
 				}
 			}
 		});
+
+		// Add item listener to the mutations check box
 		mutationsCheckBox.addItemListener(new ItemListener() {
 			@Override
 			public void itemStateChanged(ItemEvent e) {
+				// Enable/disable interval text field based on check box selection
 				switch (e.getStateChange()) {
 					case ItemEvent.SELECTED:
 						intervalTextField.setEnabled(true);
@@ -51,8 +72,12 @@ public class WordleSolverGUI {
 		});
 	}
 
+	/**
+	 * Displays the Wordle Solver GUI.
+	 */
 	public static void display() {
-		JFrame frame = new JFrame("WordleSolverGUI");
+		// Create and configure the JFrame
+		JFrame frame = new JFrame("Wordle Solver"); // Set the window title to "Wordle Solver"
 		frame.setContentPane(new WordleSolverGUI().mainPanel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
